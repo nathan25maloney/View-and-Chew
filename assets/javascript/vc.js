58992b0a707e947caac072fb753afc3474d5b5b1
Nathan Maloney <Nathan Maloney>
2017-05-23 20:50:04 -0700

    first push

---
+++ assets/javascript/vc.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyCWOSIGhQZ9E4xJ3f8HpZu8QsJiyCiD8f4",
+    authDomain: "group-project-305bb.firebaseapp.com",
+    databaseURL: "https://group-project-305bb.firebaseio.com",
+    projectId: "group-project-305bb",
+    storageBucket: "group-project-305bb.appspot.com",
+    messagingSenderId: "898983937765"
+ };
+ firebase.initializeApp(config);
+
+ 
+
