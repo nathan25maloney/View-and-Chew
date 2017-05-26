7ece1afcd6580445374b49bf9b12f14d34353f1b
Saul Perea-Garayzar <saulpg@Sauls-MacBook-Air.local>
2017-05-25 21:21:19 -0700

    Submit Btn Functionality

--- assets/javascript/vc.js
+++ assets/javascript/vc.js
@@ -6,65 +6,71 @@ var config = {
     projectId: "group-project-305bb",
     storageBucket: "group-project-305bb.appspot.com",
     messagingSenderId: "898983937765"
- };
- firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 
 
- function eventSearch(whatArg,whenArg,whereArg) {
- 	var latLong = "";
- 	 var geocoder =  new google.maps.Geocoder();
-	    geocoder.geocode( { 'address': whereArg}, function(results, status) {
-	          if (status == google.maps.GeocoderStatus.OK) {
-	          	console.log(results[0].geometry.location.lat());
-	          	console.log(results[0].geometry.location.lng());
-	            latLong =results[0].geometry.location.lat()+"," +results[0].geometry.location.lng();
-	            console.log(latLong);
+function eventSearch(whatArg, whenArg, whereArg) {
+    var latLong = "";
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 'address': whereArg }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            console.log(results[0].geometry.location.lat());
+            console.log(results[0].geometry.location.lng());
+            latLong = results[0].geometry.location.lat() + "," + results[0].geometry.location.lng();
+            console.log(latLong);
 
 
-	            var latLongString = latLong.toString();  
-				console.log("the string of lat and long ",latLongString); 
-				var when = whenArg;
-				var what = whatArg;
-			 	var eventQueryURL = "https://app.ticketmaster.com/discovery/v2/events.json?latlong="+latLongString+"&endDateTime="+when +"T23:59:59Z&classificationName="+what+"&apikey=FPzWHn2ZYdKIpOOHgDw7rZMZpISVYwdG";
+            var latLongString = latLong.toString();
+            console.log("the string of lat and long ", latLongString);
+            var when = whenArg;
+            var what = whatArg;
+            var eventQueryURL = "https://app.ticketmaster.com/discovery/v2/events.json?latlong=" + latLongString + "&endDateTime=" + when + "T23:59:59Z&classificationName=" + what + "&apikey=FPzWHn2ZYdKIpOOHgDw7rZMZpISVYwdG";
 
-			 	$.ajax({
-				    url: eventQueryURL,
-				    method: "GET"
-				}).done(function(response) { 
-					console.log("this is ticketmasters response ",response);
+            $.ajax({
+                url: eventQueryURL,
+                method: "GET"
+            }).done(function(response) {
+                console.log("this is ticketmasters response ", response);
 
-				});
-	          } else {
-	            alert("Something got wrong " + status);
-	          }
-	        });
-
-
- }
- 
+            });
+        } else {
+            alert("Something got wrong " + status);
+        }
+    });
 
 
+}
 
 
 
- // $.ajax({
- //        url: yelpQueryURL,
- //        method: "GET"
- //    }).done(function(response) { 
-
-
- //    });
 
 
 
-    $(document).ready( function(){
-    	eventSearch("music","2017-05-30","chandler, az");
-    	$("#search-btn").on("click", function(){
+// $.ajax({
+//        url: yelpQueryURL,
+//        method: "GET"
+//    }).done(function(response) { 
 
-		}); 
+
+//    });
+
+
+
+$(document).ready(function() {
+    
+    $("#addChar").on("click", function(e) {
+    	e.preventDefault();
+
+    	var a = $("#whatEvent").val().trim();
+    	var b = $("#whenEvent").val().trim();
+    	var c = $("#whereEvent").val().trim();
+
+    	console.log(a, b, c);
+
+    	eventSearch();
 
     });
 
- 
-
+});
