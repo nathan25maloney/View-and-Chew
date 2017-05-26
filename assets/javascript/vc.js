87829e10bcdc0c9da88c08c48d858000ef248e2a
Nathan Maloney <Nathan Maloney>
2017-05-25 20:27:01 -0700

    starting work on the ticketmaster api call

--- assets/javascript/vc.js
+++ assets/javascript/vc.js
@@ -8,5 +8,62 @@ var config = {
  };
  firebase.initializeApp(config);
 
+
+
+ function eventSearch(whatArg,whenArg,whereArg) {
+ 	var latLong = "";
+ 	 var geocoder =  new google.maps.Geocoder();
+	    geocoder.geocode( { 'address': whereArg}, function(results, status) {
+	          if (status == google.maps.GeocoderStatus.OK) {
+	          	console.log(results[0].geometry.location.lat());
+	          	console.log(results[0].geometry.location.lng());
+	            latLong =results[0].geometry.location.lat()+"," +results[0].geometry.location.lng();
+	            console.log(latLong);
+
+
+	            var latLongString = latLong.toString();  
+				console.log("the string of lat and long ",latLongString); 
+				var when = whenArg;
+				var what = whatArg;
+			 	var eventQueryURL = "https://app.ticketmaster.com/discovery/v2/events.json?latlong="+latLongString+"&endDateTime="+when +"T23:59:59Z&classificationName="+what+"&apikey=FPzWHn2ZYdKIpOOHgDw7rZMZpISVYwdG";
+
+			 	$.ajax({
+				    url: eventQueryURL,
+				    method: "GET"
+				}).done(function(response) { 
+					console.log("this is ticketmasters response ",response);
+
+				});
+	          } else {
+	            alert("Something got wrong " + status);
+	          }
+	        });
+
+
+ }
+ 
+
+
+
+
+
+ // $.ajax({
+ //        url: yelpQueryURL,
+ //        method: "GET"
+ //    }).done(function(response) { 
+
+
+ //    });
+
+
+
+    $(document).ready( function(){
+    	eventSearch("music","2017-05-30","chandler, az");
+    	$("#search-btn").on("click", function(){
+
+		}); 
+
+    });
+
  
 
